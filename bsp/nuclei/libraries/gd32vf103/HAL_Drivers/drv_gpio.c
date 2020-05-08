b4a7fe38e937cd136409f62ab010f7415dfdefa6
Huaqi Fang <578567190@qq.com>
2020-05-08 14:44:11 +0800

    [bsp/nuclei] Add spi and i2c drivers for gd32vf103
    * Drivers are not tested, just compile ok, need to do test with sensors
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- bsp/nuclei/libraries/gd32vf103/HAL_Drivers/drv_gpio.c
+++ bsp/nuclei/libraries/gd32vf103/HAL_Drivers/drv_gpio.c
@@ -515,6 +515,7 @@ int rt_hw_pin_init(void)
     rcu_periph_clock_enable(RCU_AF);
     return rt_device_pin_register("pin", &_gd32_pin_ops, RT_NULL);
 }
-INIT_BOARD_EXPORT(rt_hw_pin_init);
+
+INIT_DEVICE_EXPORT(rt_hw_pin_init);
 
 #endif /* RT_USING_PIN */
